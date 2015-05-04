0a71ad4efb34650da4e03f9bbf0df7de5b315607
Tyler Rich <tyler@continion.com>
2015-05-04 11:43:28 -0500

    self. , 'r', create object

--- steamapi_wrapper.py
+++ steamapi_wrapper.py
@@ -7,23 +7,21 @@ class Steam_API(object):
 ## Wrapper for Steam API ##	
 # http://api.steampowered.com/<interface name>/<method name>/v<version>/?key=<api key>&format=<format>
 	def __init__(self):
-		with open('userInfo.json', r) as inFile:
-			userInfo = json.load(inFile)
+		with open('userInfo.json', 'r') as inFile:
+			self.userInfo = json.load(inFile)
 
-		api_list = 'http://api.steampowered.com/ISteamWebAPIUtil/GetSupportedAPIList/v0001/?key='
-		get_api_list = requests.get(api_list + userInfo['auth_key']).json()['apilist']['interfaces']
-		api_options = {}
-		for interface_name in get_api_list:
-			api_options[interface_name['name']] = interface_name['methods']
-		
-		return api_options
+		self.api_list = 'http://api.steampowered.com/ISteamWebAPIUtil/GetSupportedAPIList/v0001/?key='
+		self.get_api_list = requests.get(api_list + userInfo['auth_key']).json()['apilist']['interfaces']
+		self.api_options = {}
+		for interface_name in self.get_api_list:
+			self.api_options[interface_name['name']] = interface_name['methods']
 
-	def call_api(interface, method=None):
+	def call_api(self, interface, method=None):
 	    interface_options = {}
-	    if api_options[interface]:
+	    if self.api_options[interface]:
 	        print interface
-	        for x, y in list(enumerate(api_options[interface])):
+	        for x, y in list(enumerate(self.api_options[interface])):
 	            interface_options[y['name']] = x
 
-
-print call_api('IDOTA2Fantasy_205790')
+s = Steam_API()
+print s.call_api('IDOTA2Fantasy_205790')
