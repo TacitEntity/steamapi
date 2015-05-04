1f4882db4ae4d743b35b479dcdad602b62b9a266
TacitEntity <SLNapster02@gmail.com>
2015-05-04 15:20:50 -0500

    Finishing up with call_api. Need to work out argument specification.

--- steamapi_wrapper.py
+++ steamapi_wrapper.py
@@ -16,12 +16,28 @@ class Steam_API(object):
 		for interface_name in self.get_api_list:
 			self.api_options[interface_name['name']] = interface_name['methods']
 
+	def get_interface_names(self):
+		for x in self.api_options:
+			print x
+
 	def call_api(self, interface, method=None):
 	    interface_options = {}
 	    if self.api_options[interface]:
-	        print interface
 	        for x, y in list(enumerate(self.api_options[interface])):
 	            interface_options[y['name']] = x
 
-s = Steam_API()
-print s.call_api('IDOTA2Fantasy_205790')
+	    if method is None:
+	    	# If no method argument is supplied, print all available method options for the interface.
+	    	for x in interface_options:
+	    		print x
+		else:
+			# with interface, method, interface_options[method], build off get_api_list to capture version.
+			# print 'http://api.steampowered.com/%s/%s/v%s/?' % (interface, method, interface_options[method])
+			print 'Testing (wtf else the first condition went through!)'
+
+## Testing... going to finish the query build for this function.
+## Advanced steam api queries will call to this query builder in later functions.
+
+steam = Steam_API()
+print steam.call_api('ICSGOServers_730')
+# testing method=None with the second steam.call_api argument: 'GetGameServersStatus'
